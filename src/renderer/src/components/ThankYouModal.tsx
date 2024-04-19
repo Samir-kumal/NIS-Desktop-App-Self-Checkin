a4b0fa812dc90f1a6df9b39d5224e0717879ca22
Samir-kumal <kumalsameer124@gmail.com>
2024-04-19 16:22:16 +0545

    feat: added thankyou modal

---
+++ src/renderer/src/components/ThankYouModal.tsx
@@ -0,0 +1,29 @@
+import { motion } from 'framer-motion'
+const ThankYouModal = ({ participantData }) => {
+
+  return (
+    <div className="grid place-content-center h-screen w-fit m-auto px-4    mb-10 justify-center items-center">
+      <motion.div
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ type: 'spring', delay: 0.1 }}
+        className="h-[50vh] bg-[#1D4389] flex flex-col items-center justify-center px-20  shadow-lg p-4  "
+      >
+        <h1 className="text-4xl  text-white font-bold text-center mt-0 mb-5 ">
+          Thank you for Participating
+        </h1>
+
+        <h1 className="text-4xl text-white font-bold text-center mt-0 mb-5 ">in</h1>
+        <h1 className="text-4xl text-white  font-bold text-center mt-0 mb-5 ">
+          Nepal Investment Summit 2024
+        </h1>
+        <h1 className="text-4xl text-white font-bold text-center mt-0 mb-5 ">
+          {participantData.title} {participantData.full_name}
+        </h1>
+        <img src="https://investinnepal.gov.np/wp-content/themes/eventeo/assets/images/line_inner.png" alt="logo" className="h-auto w-1/3 " />
+      </motion.div>
+    </div>
+  )
+}
+
+export default ThankYouModal
