0a25ceb5750b017bf5474dd3912f2b47542c4931
Samir-kumal <kumalsameer124@gmail.com>
2024-04-21 12:32:11 +0545

    fix: spelling of the utility function

--- src/renderer/src/utility/seperateChar.ts
+++ src/renderer/src/utility/seperateChar.ts
@@ -1,4 +1,4 @@
-export const seperateChar = (str: string, char: string) => {
+export const separateChar = (str: string, char: string) => {
     const value = str.split(char)[0]
     const upperValue = value.toUpperCase()
   return upperValue
